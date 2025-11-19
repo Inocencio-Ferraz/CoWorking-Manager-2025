9eb4949104381b3049747c9b9a7de9bdd2cf4e78
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-11-19 20:22:17 -0300

    Implement EspacoDAO class with methods

--- CoWorking_Manager/CoWorking_Manager/src/dao/EspacoDAO.java
+++ CoWorking_Manager/CoWorking_Manager/src/dao/EspacoDAO.java
@@ -1,5 +1,22 @@
 package dao;
 
-public class EspacoDAO {
+public class EspacoDAO extends baseDAO{
+
+	public EspacoDAO(String fileName, Class type) {
+		super(fileName, type);
+		
+	}
+
+	@Override
+	public void salvar() {
+		
+		
+	}
+
+	@Override
+	public void excluir() {
+		
+		
+	}
 
 }
