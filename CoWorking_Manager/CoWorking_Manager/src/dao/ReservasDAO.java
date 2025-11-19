1357f2bd0b5ebc6f504d120b499a229e901736a2
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-11-19 20:21:34 -0300

    Extend ReservasDAO from baseDAO with methods

--- CoWorking_Manager/CoWorking_Manager/src/dao/ReservasDAO.java
+++ CoWorking_Manager/CoWorking_Manager/src/dao/ReservasDAO.java
@@ -1,5 +1,22 @@
 package dao;
 
-public class ReservasDAO {
+public class ReservasDAO extends baseDAO{
+
+	public ReservasDAO(String fileName, Class type) {
+		super(fileName, type);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void salvar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
