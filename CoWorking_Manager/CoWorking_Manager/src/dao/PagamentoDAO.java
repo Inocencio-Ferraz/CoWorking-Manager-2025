f891e4a72b04c51bc8f53fda2c8eab0450774f9e
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-11-19 20:22:04 -0300

    Implement PagamentoDAO class extending baseDAO

--- CoWorking_Manager/CoWorking_Manager/src/dao/PagamentoDAO.java
+++ CoWorking_Manager/CoWorking_Manager/src/dao/PagamentoDAO.java
@@ -1,6 +1,22 @@
 package dao;
 
-public class PagamentoDAO {
+public class PagamentoDAO extends baseDAO {
+
+	public PagamentoDAO(String fileName, Class type) {
+		super(fileName, type);
+	
+	}
+
+	@Override
+	public void salvar() {
+		
+	}
+
+	@Override
+	public void excluir() {
+		
+		
+	}
 
 	
 }
