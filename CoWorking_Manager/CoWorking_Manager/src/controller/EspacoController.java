e9990a4d8639cd20de0a53acc7be947c68d1b391
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-11-22 18:15:32 -0300

    Implement cadastroDeEspaco method in EspacoController
    Added method for registering a space in EspacoController.

--- CoWorking_Manager/CoWorking_Manager/src/controller/EspacoController.java
+++ CoWorking_Manager/CoWorking_Manager/src/controller/EspacoController.java
@@ -1,5 +1,17 @@
 package controller;
 
+import model_.Espaco;
+
+// oq fazer aqui cadastrar, editar, listar, remover, buscar po ID
+
+
 public class EspacoController {
+	
+	public void cadastroDeEspaco() {
+		
+	}
+	
+	
+	
 
 }
