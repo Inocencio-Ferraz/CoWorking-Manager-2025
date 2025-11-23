5c27ff1f2667e30749e4ec16df7c6a6fc656f64d
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-11-23 03:33:31 -0300

    Update EspacoController.java

--- CoWorking_Manager/CoWorking_Manager/src/controller/EspacoController.java
+++ CoWorking_Manager/CoWorking_Manager/src/controller/EspacoController.java
@@ -1,17 +1,33 @@
 package controller;
 
+import java.time.LocalDateTime;
+
+import dao.EspacoDAO;
+import dao.PagamentoDAO;
+import model_.Auditorio;
+import model_.CabineIndividual;
 import model_.Espaco;
+import model_.Pagamento;
+import model_.SalaDeReuniao;
 
 // oq fazer aqui cadastrar, editar, listar, remover, buscar po ID
 
 
 public class EspacoController {
 	
-	public void cadastroDeEspaco() {
-		
+	EspacoDAO espacoDAO = new EspacoDAO();
+	PagamentoDAO pagamentoDAO = new PagamentoDAO();
+	
+	public void salvarSalaDeReuniao(String name, Integer capacidade, Double precoPorHora) {	
+		Espaco espaco = new SalaDeReuniao();	
+		espacoDAO.salvar(espaco);
 	}
 	
-	
+	public void salvarCabineIndividual(String name, Integer capacidade, Double precoPorHora) {	
+		Espaco espaco = new CabineIndividual();	
+		espacoDAO.salvar(espaco);
+	}
 	
 
 }
+
