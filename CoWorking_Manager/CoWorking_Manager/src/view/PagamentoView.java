efd36b7bcb7972e31888ea9b0d965a14a23f0bb4
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-12-04 18:25:28 -0300

    Refactor PagamentoView to PagamentoViews with updates

--- CoWorking_Manager/CoWorking_Manager/src/view/PagamentoView.java
+++ CoWorking_Manager/CoWorking_Manager/src/view/PagamentoView.java
@@ -1,5 +1,57 @@
-package view;
+package Views;
 
-public class PagamentoView {
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+import controller.PagamentoController;
+
+public class PagamentoViews {
+	
+	public PagamentoViews(){
+	}
+	
+	public void operacaoPagamentoViews() {
+		
+		Scanner sc = new Scanner(System.in);
+		Gson gson = new Gson();
+
+		
+		System.out.print("\n[1] Registrar Pagamento\n[2] Lista de Pagamento\n[3] Buscar por ID\n[4] Excluir Pagamento\n[5] Voltar ao menu");
+		System.out.print("\nOpção desejada: ");
+		int op = sc.nextInt();
+		
+		if(op == 1) {
+			new PagamentoController() ;
+			System.out.println("Realizar Pagamento.");
+			System.out.println("Qual e o ID da reseva: ");
+			int id = sc.nextInt();
+			System.out.print("Formas de Pagamento. \nPIX\nCARTAO\nDINHEIRO");
+			System.out.print("Qual e a forma de pagamento? ");
+			String metodo = sc.next();
+			new PagamentoController().registrarPagamento(id, metodo);
+		}
+		else if(op == 2) {
+			
+			new PagamentoController().listarPagamentos();
+		}
+		else if(op == 3) { 
+			
+			System.out.print("Id da reserva que deseja buscar: ");
+			int id = sc.nextInt();
+			new PagamentoController().buscarPorId(id);
+			
+		}
+		else if(op == 4) {
+			System.out.print("Id da reserva que deseja excluir: ");
+			int id = sc.nextInt();
+			new PagamentoController().excluirPagamento(id);
+			
+		}
+		else if (op == 5) {
+			return;	
+		}
+	}
+	
 
 }
