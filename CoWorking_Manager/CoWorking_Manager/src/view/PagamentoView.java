74323e8ade7e0c6e18c74424d51b46cce23a2bae
Clebio-Luis <100530708+clebio0901@users.noreply.github.com>
2025-12-07 00:40:04 -0300

    Refactor PagamentoView to PagamentoViews and update methods

--- CoWorking_Manager/CoWorking_Manager/src/view/PagamentoView.java
+++ CoWorking_Manager/CoWorking_Manager/src/view/PagamentoView.java
@@ -1,22 +1,26 @@
-package view;
+package Views;
 
 import java.util.Scanner;
 
 import com.google.gson.Gson;
 
 import controller.PagamentoController;
+import dao.PagamentoDAO;
+import model_.Pagamento;
 
-public class PagamentoView {
+public class PagamentoViews {
 	
-	public PagamentoView(){
+	public PagamentoViews(){
 	}
 	
 	public void operacaoPagamentoViews() {
 		
 		Scanner sc = new Scanner(System.in);
-		Gson gson = new Gson();
+		//Gson gson = new Gson();
 
-		
+		while(true) {
+			
+		System.out.println("GERENCIAR PAGAMENTOS");
 		System.out.print("\n[1] Registrar Pagamento\n[2] Lista de Pagamento\n[3] Buscar por ID\n[4] Excluir Pagamento\n[5] Voltar ao menu");
 		System.out.print("\nOpção desejada: ");
 		int op = sc.nextInt();
@@ -27,19 +31,23 @@ public class PagamentoView {
 			System.out.println("Qual e o ID da reseva: ");
 			int id = sc.nextInt();
 			System.out.print("Formas de Pagamento. \nPIX\nCARTAO\nDINHEIRO");
-			System.out.print("Qual e a forma de pagamento? ");
+			System.out.print("\nQual e a forma de pagamento? ");
 			String metodo = sc.next();
+			/* jeito certo de salvar o pagamento 
+			 * PagamentoDAO dao3 = new PagamentoDAO(); dao3.salvar(new Pagamento(4, 12,
+			 * 20.0, java.time.LocalDateTime.now(), enums.Pagamento.PIX));
+			 */
 			new PagamentoController().registrarPagamento(id, metodo);
 		}
 		else if(op == 2) {
 			
-			new PagamentoController().listarPagamentos();
+		System.out.println(new PagamentoController().listarPagamentos());
 		}
 		else if(op == 3) { 
 			
 			System.out.print("Id da reserva que deseja buscar: ");
 			int id = sc.nextInt();
-			new PagamentoController().buscarPorId(id);
+			System.out.println(new PagamentoController().buscarPorId(id));
 			
 		}
 		else if(op == 4) {
@@ -49,9 +57,13 @@ public class PagamentoView {
 			
 		}
 		else if (op == 5) {
-			return;	
+			
+            break;
+			
 		}
 	}
-	
+
+		sc.close();
+	}
 
 }
